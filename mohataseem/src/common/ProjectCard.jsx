b106f379d12bd33cf4b19700475a1961c04167bc
Mohataseem Khan <mohataseem89@gmail.com>
2025-03-01 16:19:03 +0530

    Update ProjectCard.jsx

--- mohataseem/src/common/ProjectCard.jsx
+++ mohataseem/src/common/ProjectCard.jsx
@@ -1,13 +1,22 @@
+
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function ProjectCard({ src, link, h3, p }) {
   return (
-    <a href={link} target="_blank">
-      <img className="hover" src={src} alt={`${h3} logo`} />
+    <a href={link} target="_blank" rel="noopener noreferrer" aria-label={h3}>
+      <img className="hover" src={src} alt={`${h3} project logo`} />
       <h3>{h3}</h3>
       <p>{p}</p>
     </a>
   );
 }
+
+ProjectCard.propTypes = {
+  src: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  h3: PropTypes.string.isRequired,
+  p: PropTypes.string.isRequired,
+};
 
 export default ProjectCard;
